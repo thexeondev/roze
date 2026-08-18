650290762673c57c65d03359cc062b7c369f13b5
xeon <xeon@xeondev.com>
2026-08-18 16:50:40 +0300

    CS_LEAVE_BATTLE: handle failure case

--- src/commands/battle.zig
+++ src/commands/battle.zig
@@ -4,12 +4,10 @@ const roze = @import("../roze.zig");
 const protobuf = roze.protobuf;
 const Gameplay = roze.Gameplay;
 const Scope = roze.Scope;
-
-const log = std.log.scoped(.@"roze::commands::battle");
+const assets = roze.assets;
 
 pub fn CS_ENTER_BATTLE(scope: *Scope) Scope.Error!void {
     const request = try scope.command.take(protobuf.gen.CS_Enter_Battle.Decoded);
-    log.debug("received enter battle request: {any}", .{request});
 
     try scope.sink.send(.SC_ENTER_BATTLE, @as(
         protobuf.gen.SC_Enter_Battle,
@@ -25,8 +23,6 @@ pub fn CS_ENTER_BATTLE(scope: *Scope) Scope.Error!void {
 pub fn CS_START_BATTLE(scope: *Scope) Scope.Error!void {
     const request = try scope.command.take(protobuf.gen.CS_Start_Battle.Decoded);
 
-    log.debug("received start battle request: {any}", .{request});
-
     try scope.sink.send(.SC_START_BATTLE, @as(
         protobuf.gen.SC_Start_Battle,
         .{
@@ -38,24 +34,41 @@ pub fn CS_START_BATTLE(scope: *Scope) Scope.Error!void {
 }
 
 pub fn CS_LEAVE_BATTLE(scope: *Scope) Scope.Error!void {
+    const log = std.log.scoped(.@"roze::commands::leave_battle");
+
     var request = try scope.command.take(protobuf.gen.CS_Leave_Battle.Decoded);
+    var state_save_needed: bool = false;
 
-    log.debug("received leave battle request: {any}", .{request});
+    const battle_result = request.battle_result orelse {
+        log.debug("invalid battle result", .{});
+        return try scope.sink.send(.SC_LEAVE_BATTLE, @as(
+            protobuf.gen.SC_Leave_Battle,
+            .{ .ret = -1 },
+        ));
+    };
 
-    if (request.battle_end_info) |*end_info| {
-        log.debug("battle_end_info.vector3: {any}", .{end_info.vector3});
-    }
-
-    var should_save_state: bool = false;
+    const hp_needs_refill = switch (battle_result) {
+        .ENM_BATTLE_RESULT_TYPE_DEAD_FAIL => true,
+        else => false,
+    };
 
     const character = &scope.gameplay.character;
     while (try request.character_data.next()) |character_data| {
         const character_index = Gameplay.Character.Index.fromInstId(character_data.inst_id) orelse continue;
+
+        const hp_new = if (hp_needs_refill) hp_max: {
+            const bits = character.bits[@backingInt(character_index)];
+            const dev_attributes = scope.asset_index.getDevelopAttributes(character_index);
+
+            break :hp_max dev_attributes.maxhp *
+                assets.getScaleForLevel(.scale_maxhp, bits.level, bits.break_level);
+        } else character_data.current_hp;
+
         character.snapshots[character_index.toInt()] = .wrap(.{
-            .hp = character_data.current_hp,
+            .hp = hp_new,
             .permanent_liquid = character_data.permanent_liquid,
         });
-        should_save_state = true;
+        state_save_needed = true;
 
         {
             var attr_buffer: protobuf.packers.Buffer(
@@ -79,6 +92,14 @@ pub fn CS_LEAVE_BATTLE(scope: *Scope) Scope.Error!void {
         }
     }
 
+    const extra_info: ?protobuf.gen.BattleExtraInfo = switch (battle_result) {
+        .ENM_BATTLE_RESULT_TYPE_DEAD_FAIL => .{
+            .reborn_map_id = scope.gameplay.map.id,
+            .reborn_savepoint = assets.npc_lists.save_point_npc_ids[scope.gameplay.map.save_point_index],
+        },
+        else => null,
+    };
+
     try scope.sink.send(.SC_LEAVE_BATTLE, @as(
         protobuf.gen.SC_Leave_Battle,
         .{
@@ -86,10 +107,10 @@ pub fn CS_LEAVE_BATTLE(scope: *Scope) Scope.Error!void {
             .battle_type = request.battle_type,
             .battle_field_id = request.battle_field_id,
             .battle_result = request.battle_result,
-            .extra_info = null,
+            .extra_info = extra_info,
         },
     ));
 
     errdefer comptime unreachable; // Ensure that it's safe to request save.
-    if (should_save_state) scope.store.requestGameplayStateSave();
+    if (state_save_needed) scope.store.requestGameplayStateSave();
 }
